b673e8c55d0c9ac5ff4ee59dd9bf429b005f4fa8
SolidJackie <161447366+SolidJackie@users.noreply.github.com>
2024-10-07 13:26:24 +0100

    Init

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform Target;
+    NavMeshAgent nMA;
+
+    [SerializeField] float aggroRange = 5f;
+    float distanceToPlayer;
+
+    private void Start()
+    {
+        nMA = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        distanceToPlayer = Vector3.Distance(Target.position, transform.position);
+        if(distanceToPlayer <= aggroRange)
+        {
+            nMA.SetDestination(Target.transform.position);
+        }
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, aggroRange);
+    }
+}
